c97ee7b86ce92486a07d9fbe08e331b5c2c62d3d
PEIHENG(Jonathan) LIU <Liupeter012001@live.cn>
2023-10-24 21:47:14 +1100

    1.I am not familiar with springboot,so please intergrate my code into the main source 2.Please download required packages and Java client library for Google Maps API Web Services 3.LLMModel needs API key.

---
+++ LocationGetter.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.text.DecimalFormat;
+
+class LocationGetter {
+    static String AK = "AIzaSyD7GJRndyf2IDwy3IPksILwQ5Enm8sPEkk"; //key
+
+    // input scenic spot's name and get coordinate
+    public static String getCoordinate(String address) {
+        if (address != null && !"".equals(address)) {
+            address = address.replaceAll("\\s*", "").replace("#", "栋");
+            String url = "http://api.map.baidu.com/geocoder/v2/?address=" + address + "&output=json&key=" + AK;
+            String json = loadJSON(url);
+            if (json != null && !"".equals(json)) {
+                JSONObject obj = JSONObject.fromObject(json);
+                if ("0".equals(obj.getString("status"))) {
+                    double lng = obj.getJSONObject("result").getJSONObject("location").getDouble("lng"); // 经度
+                    double lat = obj.getJSONObject("result").getJSONObject("location").getDouble("lat"); // 纬度
+                    DecimalFormat df = new DecimalFormat("#.######");
+                    return df.format(lng) + "," + df.format(lat);
+                }
+            }
+        }
+        return null;
+    }
+
+    public static String loadJSON(String url) {
+        StringBuilder json = new StringBuilder();
+        try {
+            URL oracle = new URL(url);
+            URLConnection yc = oracle.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream(), "UTF-8"));
+            String inputLine = null;
+            while ((inputLine = in.readLine()) != null) {
+                json.append(inputLine);
+            }
+            in.close();
+        } catch (MalformedURLException e) {} catch (IOException e) {}
+        return json.toString();
+    }
+}
